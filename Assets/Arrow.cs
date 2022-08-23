9f0a997cb1ee2d50dc0138c59f10dfafca9a1ca5
ZEYNEP\PC <zeynepclktn@gmail.com>
2022-08-23 22:13:26 +0300

    11th and 20th missions done.
    Issue Key: NAOR-17

---
+++ Assets/Arrow.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Arrow : MonoBehaviour
+{
+
+    Transform PlayerPosition;
+    [SerializeField] private float daggerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    public static float daggerDamage = 5f;
+    public static Arrow instance;
+    float y;
+
+    public bool left;
+    public bool right;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = this.GetComponent<Rigidbody2D>();
+       
+
+        if (this.gameObject.tag=="uparrow")
+            y = PlayerPosition.position.y + 1;
+        else if(this.gameObject.tag=="downarrow")
+            y = PlayerPosition.position.y - 0.4f ;
+
+        this.gameObject.transform.position = new Vector2(this.gameObject.transform.position.x, y);
+
+        if (left) 
+        { 
+            direction = Vector2.left;
+            this.gameObject.GetComponent<SpriteRenderer>().flipX=true;
+        }
+        else if(right)
+        {
+            direction = Vector2.right;
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        Debug.Log(direction);
+        
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = direction*daggerSpeed;
+    }
+    public void Initialize(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+            //collision.transform.SendMessage("DamagePlayer", 10);
+        }
+
+    }
+}
--- Assets/InteractSystem.cs
+++ Assets/InteractSystem.cs
@@ -14,52 +14,21 @@ public class InteractSystem : MonoBehaviour
     Rigidbody2D related_rigidbody; 
     Animator anim;
     Animator anim2;
+    Transform PlayerPosition;
     bool active = false;
     Vector2 movement;
 
     void Start()
     {
-
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        movement =new Vector2(PlayerPosition.position.x - 1, PlayerPosition.position.y);
     }
     void Update()
     {
         tagControl(isInRange);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player"))
-            isInRange = true;
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isInRange = false;
-    }
-    void stairsActive()
-    {
-        anim.SetBool("Trigger", true);
-    }
-    void stairsCancel()
-    {
-        anim.SetBool("Trigger", false);
-    }
-    void platformActive()
-    {
 
-        active = true;
-        anim2.SetBool("trig", true);
-        anim.SetBool("Trigger", true);
 
-        StartCoroutine(backtoIdle());
-    }
-    IEnumerator backtoIdle()
-    {
-        yield return new WaitForSeconds(4);
-        anim.SetBool("Trigger", false);
-        anim2.SetBool("trig", false);
-        active = false;
-        isInRange = false;
-    }
     void tagControl(bool isInRange)
     {
         if (this.tag == "stairs")
@@ -90,6 +59,46 @@ public class InteractSystem : MonoBehaviour
             if (isInRange && active) //etki alanýndaysa ve hareket yeteneðini aktif etmiþse iliþkili event fonksiyonunu çaðýr yani boxcontrol
                 interactAction.Invoke();
         }
+        else if(this.tag=="music")
+        {
+            if (isInRange && Input.GetKeyDown(interactKey))
+                interactAction.Invoke();
+           
+        }
+        else if(this.tag=="arrowtrigger")
+        {
+            if(isInRange && !active)
+            {
+                active = true;
+                //related_object.SetActive(true);
+                interactAction.Invoke();
+            }
+        }
+    }
+    void stairsActive()
+    {
+        anim.SetBool("Trigger", true);
+    }
+    void stairsCancel()
+    {
+        anim.SetBool("Trigger", false);
+    }
+    void platformActive()
+    {
+
+        active = true;
+        anim2.SetBool("trig", true);
+        anim.SetBool("Trigger", true);
+
+        StartCoroutine(backtoIdle());
+    }
+    IEnumerator backtoIdle()
+    {
+        yield return new WaitForSeconds(4);
+        anim.SetBool("Trigger", false);
+        anim2.SetBool("trig", false);
+        active = false;
+        isInRange = false;
     }
     public void BoxControl()
     {
@@ -99,5 +108,15 @@ public class InteractSystem : MonoBehaviour
         movement.y= Input.GetAxisRaw("Vertical");
         related_rigidbody.MovePosition(related_rigidbody.position + movement * 0.5f * Time.fixedDeltaTime);
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            isInRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            isInRange = false;
     }
 }
