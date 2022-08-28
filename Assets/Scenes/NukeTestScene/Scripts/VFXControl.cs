2354b5a56a2e90e8e7cdd043072bac93414c5be2
DESKTOP-JH2KCUA\PC <aabdullahtekin@gmail.com>
2022-08-28 05:00:11 +0300

    -Baştan bi elden geçirdim yeni şeyler ekledim -Partikülleri 6 derece rotateledim ortographic açıdan güzel gözüksün diye -Şu an patlama sesini ekleyebildim meteor düşme sesi yok -Dumanları tam halledemedim ama fena durmuyor yine de -Yüksekten düşen meteorun shaderı spawnlandığı noktaya bakan bir kamera olmadan çalışmadığından 2. kamerayı ekledim o açıyı alan.
    Issue Key: NAOR-4

--- Assets/Scenes/NukeTestScene/Scripts/VFXControl.cs
+++ Assets/Scenes/NukeTestScene/Scripts/VFXControl.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class VFXControl : MonoBehaviour
 {
     public float        nukeDelay;
-    public GameObject   meteor;
-    public GameObject   nuke;
+    public GameObject   meteorVFX;
+    public GameObject   nukeVFX;
+    public GameObject   audioSourceObject;
+    public AudioClip    nukeSFX;
+    //public AudioClip    meteorSFX;
+    private AudioSource  audioSource;
 
     void Start()
     {
-        
+        audioSource = audioSourceObject.transform.GetComponent<AudioSource>();
     }
 
     void Update()
@@ -18,7 +22,8 @@ public class VFXControl : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Nuke());
-            GameObject vfxmeteor = Instantiate(meteor) as GameObject;
+            //audioSource.PlayOneShot(meteorSFX);
+            GameObject vfxmeteor = Instantiate(meteorVFX) as GameObject;
             Destroy(vfxmeteor, nukeDelay);
         }
     }
@@ -26,7 +31,8 @@ public class VFXControl : MonoBehaviour
     IEnumerator Nuke()
     {
         yield return new WaitForSeconds(nukeDelay);
-        GameObject vfxNuke = Instantiate(nuke) as GameObject;
+        audioSource.PlayOneShot(nukeSFX);
+        GameObject vfxNuke = Instantiate(nukeVFX) as GameObject;
         Destroy(vfxNuke, 10);
     }
 }
