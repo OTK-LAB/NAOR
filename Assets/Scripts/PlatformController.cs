1c5c93f7889a5120745381fbe6fb9ffc42bfcdd4
BerhanDemiralp <Demiralpberhan@hotmail.com>
2023-02-25 17:21:23 +0300

    DOTween imported
    Issue Key : NAOR-50

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class PlatformController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
