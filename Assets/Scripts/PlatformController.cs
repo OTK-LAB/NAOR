5c723b87fa142a66257aa429076e9f57674a105a
BerhanDemiralp <Demiralpberhan@hotmail.com>
2023-03-04 15:44:45 +0300

    Platform controller system created and crushing trap completed
    Issue Key : NAOR-50

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -6,7 +6,10 @@ using DG.Tweening;
 
 public class PlatformController : MonoBehaviour
 {
-    
+    public GameObject TargetObj;
+    private bool isInRange = false;
+    private bool loop;
+
     void Start()
     {
         
@@ -14,6 +17,37 @@ public class PlatformController : MonoBehaviour
 
     void Update()
     {
+        Sequence mySequence = DOTween.Sequence();
+        if (isInRange && !loop)
+        {
+            loop= true;
+            mySequence.Append(TargetObj.transform.DOLocalMoveY(-2, 0.5f).SetEase(Ease.InBack));
+            mySequence.Append(TargetObj.transform.DOLocalMoveY(0, 4).SetEase(Ease.InQuad));
+            mySequence.SetLoops(100, LoopType.Restart);
+
+        }
         
+
+    }
+
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            isInRange = true;
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            isInRange = false;
+            loop= false;
+        }
     }
 }
