64b67c9e5445cf7a31296ea0463dd6b4852695de
IremK <ayseirem.kayacioglu@gmail.com>
2023-03-18 21:49:43 +0300

    Add down button to exit stairs Issue Key: NAOR-54

--- Assets/Stairs.cs
+++ Assets/Stairs.cs
@@ -1,22 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Stairs : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
-
-    private void Start()
+    public BoxCollider2D boxCollider;
+    public PlayerInputActions inputActions;
+    bool downCalled = false;
+    void Awake()
     {
         // Get the BoxCollider2D component of the stairs object
-        boxCollider = GetComponent<BoxCollider2D>();
+
+        // Set up the player input controls
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable();
+        inputActions.Player.Down.started += OnDownStarted;
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
-            boxCollider.enabled = false;
+            // Check if the player is jumping (by checking the y velocity)
+            if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y < 0 && !downCalled)
+            {
+                // Activate the box collider when the player jumps on the stairs
+                boxCollider.enabled = true;
+            }
+            else
+            {
+                // Deactivate the box collider when the player is walking normally
+                boxCollider.enabled = false;
+            }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Deactivate the box collider when the player leaves the stairs
+            boxCollider.enabled = false;
+            downCalled = false;
+        }
+    }
+
+   void OnDownStarted(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnDownStarted() called");
+        if(boxCollider.enabled)
+            downCalled = true;
+        // Disable the box collider when the player presses the down arrow key
+        //boxCollider.enabled = false;
+ 
     }
 }
