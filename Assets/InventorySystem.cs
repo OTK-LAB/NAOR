28fb001ff030ecb1ea590a5fcb6bf485b2c8e616
BerhanDemiralp <Demiralpberhan@hotmail.com>
2023-05-11 15:31:32 +0300

    Inventory set
    Issue Key : NAOR-60

---
+++ Assets/InventorySystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
