a22b7f199bcb0459ad48254792e8de2e55053948
boraswim <sevimbora@gmail.com>
2023-08-28 19:33:03 +0300

    Cameras set, visual improvements

--- Assets/DashGiver.cs
+++ Assets/DashGiver.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class DashGiver : MonoBehaviour
 {
     public PlayerMain player;
+    public GameObject dashUI;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             player.PlayerData.Dash.CanDash = true;
+            Time.timeScale = 0;
+            dashUI.SetActive(true);
+            //WAIT 3 SECONDS
+            dashUI.SetActive(false);
+            Time.timeScale = 1;
             Destroy(gameObject);
         }
     }
-    
 }
