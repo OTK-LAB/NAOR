d028fd1f2dc4fa2ed6560db6ba4ac2d46cdf3c98
boraswim <sevimbora@gmail.com>
2023-08-29 01:10:37 +0300

    build version

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.InputSystem;
-using Unity.VisualScripting.ReorderableList;
+//using Unity.VisualScripting.ReorderableList;
 using UnityEditor;
 
 
