8825e5772f5a4188ce8868507235dbff2a08b421
Kenan <qei@duck.com>
2023-09-14 16:32:48 +0300

    Micheal (boss) artık charge atıyor Very cool.
    Issue Key: NAOR-104

--- Assets/Scripts/Enemies/Boss1 Enemy/Boss1Manager.cs
+++ Assets/Scripts/Enemies/Boss1 Enemy/Boss1Manager.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Boss1Manager : MonoBehaviour
 {
     [HideInInspector] public GameObject Player;
+    [HideInInspector] public Transform Target;
     private Animator anim;
     private Rigidbody2D rigid;
+    public GameObject dashSmoke;
+
 
     public float moveSpeed;
     private bool notDead;
     public bool canAttack;
     private bool InAnimation;
     [HideInInspector] public bool stunned;
+    [HideInInspector] public bool charging;
 
     public float setmeleeWaitTime;
     private float meleeWaitTime;
     public float meleerange;
+
+    //[Header("Skills")]
+
+    [HideInInspector] public bool inSkillUse;
+
+    public float setchargeSkillTime;
+    private float chargeSkillTime;
+    [HideInInspector] public bool backingUpTimer;
+    private Vector2 chargingDir;
 
     //disable attack hitbox if non damage move
 
@@ -30,21 +43,42 @@ public class Boss1Manager : MonoBehaviour
         notDead = true;
         canAttack = false;
         InAnimation = false;
+        charging = false;
+        inSkillUse = false;
+        backingUpTimer = false;
 
-
+        chargeSkillTime = setchargeSkillTime;
+        meleeWaitTime = setmeleeWaitTime;
     }
 
 
     private void FixedUpdate()
     {
-        if (canAttack && Player.transform.position.x - transform.position.x < 0)
-            GetComponent<SpriteRenderer>().flipX = true;
-        else
-            GetComponent<SpriteRenderer>().flipX = false;
+        if (!stunned && !charging)
+        {
+            if (canAttack && Player.transform.position.x - transform.position.x < 0)
+            {
+                dashSmoke.GetComponent<SpriteRenderer>().flipX = false;
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                dashSmoke.GetComponent<SpriteRenderer>().flipX = true;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+
+
+        //timers
 
         if (meleeWaitTime > 0)
         {
             meleeWaitTime -= Time.deltaTime;
+        }
+
+        if (chargeSkillTime > 0)
+        {
+            chargeSkillTime -= Time.deltaTime;
         }
     }
 
@@ -52,67 +86,119 @@ public class Boss1Manager : MonoBehaviour
 
     void Update()
     {
-        if (notDead)
+        if (notDead && !stunned)
         {
-
-            if (stunned)
-            {
-
-            }
-
-
 
             if (canAttack)
             {
-                //how far is the player
+                //skill stuff
 
-                float distance = Vector2.Distance(Player.transform.position, transform.position);
+                if (backingUpTimer)
+                {
+                    Vector2 directionTarget = Target.position - transform.position;
+                    rigid.MovePosition((Vector2)transform.position + (directionTarget * moveSpeed * 7 * Time.deltaTime));
+                }
+
+                if (charging)
+                {
+                    float testingIfDamaged = Player.GetComponent<HealthSystem>().currentHealth;
+                    if (testingIfDamaged > Player.GetComponent<HealthSystem>().currentHealth)
+                    {
+                        anim.Play("flex");
+                        charging = false;
+                    }
+
+                    rigid.MovePosition((Vector2)transform.position + (chargingDir * moveSpeed * 5 * Time.deltaTime));
+                }
 
                 //moveset
 
-                if (distance < meleerange)
+                if (!inSkillUse)
                 {
-                    if (meleeWaitTime <= 0)
+                    //how far is the player
+
+                    float distance = Vector2.Distance(Player.transform.position, transform.position);
+
+                    if (distance < meleerange)
                     {
-                        //attackup/down
-                        anim.Play("attackup");
-                        meleeWaitTime = setmeleeWaitTime;
+                        if (meleeWaitTime <= 0)
+                        {
+                            //attackup/down
+                            anim.Play("attackup");
+                            meleeWaitTime = setmeleeWaitTime + 5f;
+                        }
+                        else if (!InAnimation)
+                        {
+                            anim.Play("idle");       //this can be placed with another move so he doesnt wait on our head
+                        }
                     }
-                    else if (!InAnimation)
+                    else if (distance > meleerange && !InAnimation)
                     {
-                        anim.Play("idle");       //this can be placed with another move so he doesnt wait on our head
+
+                        if (!InAnimation && (chargeSkillTime <= 0))
+                        {
+                            StartCoroutine(Charge());
+                            chargeSkillTime = setchargeSkillTime;
+                        }
+                        else
+                        {
+                            anim.Play("move");
+
+                            Vector2 direction = Player.transform.position - transform.position;
+                            rigid.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+                        }
+
+
+
+
+
                     }
-                }
-                else if (distance > meleerange)
-                {
 
-
-
-
-
-
-
-                        //if else en sonu
-                        
-                        anim.Play("move");
-
-                        Vector2 direction = Player.transform.position - transform.position;
-                        rigid.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+                    if (Player.GetComponent<HealthSystem>().currentHealth <= 0)        // for short Boss will be in a special scene so if player dies they restart the battle(SCENE)
+                        anim.Play("flex");                                             // so no need to restart boss's AI
                 }
 
-
-
-
-
-
-                if (Player.GetComponent<HealthSystem>().currentHealth <= 0)        // for short Boss will be in a special scene so if player dies they restart the battle(SCENE)
-                    anim.Play("flex");                                             // so no need to restart boss's AI
             }
             else      //not able to attack
             {
                 anim.Play("idle");
             }
+
         }
+    }
+
+    IEnumerator Charge()
+    {
+        inSkillUse = true;
+        anim.Play("backstep");
+        dashSmoke.SetActive(true);
+
+        yield return new WaitForSeconds(0.1f);   //giving time for the target set for boss
+        backingUpTimer = true;
+
+        yield return new WaitForSeconds(1.5f);
+
+        backingUpTimer = false;
+        anim.Play("willcharge");
+
+        yield return new WaitForSeconds(1.5f);
+
+        dashSmoke.SetActive(false);
+        chargingDir = Player.transform.position - transform.position;
+        charging = true;
+        anim.Play("charge");
+    }
+
+    public IEnumerator Stun()
+    {
+        charging = false;
+        stunned = true;
+
+        anim.Play("chargefail");
+        yield return new WaitForSeconds(4f);
+        inSkillUse = false;
+        meleeWaitTime = setmeleeWaitTime;
+        stunned = false;
     }
 
     public void AnimationTime(int answer)
--- Assets/Scripts/Enemies/Boss1 Enemy/Detector.cs
+++ Assets/Scripts/Enemies/Boss1 Enemy/Detector.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class Detector : MonoBehaviour
 {
+
+    public GameObject OtherBossCharge;
+
     public Boss1Manager boss1Manager;
+
+    public bool playerDetector;
     void Start()
     {
-        boss1Manager = GetComponentInParent<Boss1Manager>();
+        if (boss1Manager == null)
+        {
+            boss1Manager = GetComponentInParent<Boss1Manager>();
+        }
+
+        if (boss1Manager == null)
+        {
+            Debug.Log("Please put boss's boss1manager script into the BossChargePoints");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (!playerDetector)
         {
-            boss1Manager.Player = collision.gameObject;
-            boss1Manager.canAttack = true;
+            if (collision.tag == "Player")
+            {
+                boss1Manager.Player = collision.gameObject;
+                boss1Manager.canAttack = true;
+            }
+
+            if (collision.tag == "wall" && boss1Manager.charging)
+            {
+                StartCoroutine(boss1Manager.Stun());
+            }
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (playerDetector)
+        {
+            if (collision.tag == "Player" && boss1Manager.inSkillUse && !boss1Manager.backingUpTimer)
+            {
+                boss1Manager.Target = OtherBossCharge.transform;
+            }
         }
 
-        if (collision.tag == "wall")
-        {
-            boss1Manager.stunned = true;
-        }
     }
 }
+
