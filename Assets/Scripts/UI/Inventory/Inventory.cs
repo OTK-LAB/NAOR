5d0f97c34ff67d4f700754a8daa7fb0f15cbd8de
BerhanDemiralp <Demiralpberhan@hotmail.com>
2023-12-11 23:11:28 +0300

    Text issue and Design arranged
    Issue Key : NAOR-84-113

--- Assets/Scripts/UI/Inventory/Inventory.cs
+++ Assets/Scripts/UI/Inventory/Inventory.cs
@@ -75,18 +75,18 @@ public class Inventory : MonoBehaviour
                 {
                     if (1 <= inventory.items[i].id && inventory.items[i].id <= 5)
                     {
-                        consumableGrid.transform.GetChild(i-1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
-                        consumableGrid.transform.GetChild(i-1).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
+                        consumableGrid.transform.GetChild(0).GetChild(i-1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
+                        consumableGrid.transform.GetChild(0).GetChild(i-1).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
                     }
                     else if (6 <= inventory.items[i].id && inventory.items[i].id <= 9)
                     {
-                        throwableGrid.transform.GetChild(i-6) .gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
-                        throwableGrid.transform.GetChild(i-6).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
+                        throwableGrid.transform.GetChild(0).GetChild(i-6) .gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
+                        throwableGrid.transform.GetChild(0).GetChild(i-6).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
                     }
                     else if (13 <= inventory.items[i].id && inventory.items[i].id <= 15)
                     {
-                        permanentGrid.transform.GetChild(i - 13).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
-                        permanentGrid.transform.GetChild(i - 13).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
+                        permanentGrid.transform.GetChild(0).GetChild(i - 13).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[i].icon;
+                        permanentGrid.transform.GetChild(0).GetChild(i - 13).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventory.items[i].stack.ToString();
                     }
                 }
                 else
@@ -103,19 +103,19 @@ public class Inventory : MonoBehaviour
                     if (1 <= i && i <= 5)
                     {
                         Debug.Log("conull");
-                        consumableGrid.transform.GetChild(i - 1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
-                        consumableGrid.transform.GetChild(i - 1).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+                        consumableGrid.transform.GetChild(0).GetChild(i - 1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
+                        consumableGrid.transform.GetChild(0).GetChild(i - 1).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
                     }
                     else if (6 <= i && i <= 9)
                     {
                         Debug.Log("pernull");
-                        throwableGrid.transform.GetChild(i - 6).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
-                        throwableGrid.transform.GetChild(i - 6).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+                        throwableGrid.transform.GetChild(0).GetChild(i - 6).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
+                        throwableGrid.transform.GetChild(0).GetChild(i - 6).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
                     }
                     else if (13 <= i && i <= 15)
                     {
-                        permanentGrid.transform.GetChild(i - 13).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
-                        permanentGrid.transform.GetChild(i - 13).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+                        permanentGrid.transform.GetChild(0).GetChild(i - 13).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
+                        permanentGrid.transform.GetChild(0).GetChild(i - 13).gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
                     }
                 }
                 else
@@ -134,22 +134,38 @@ public class Inventory : MonoBehaviour
             throwableEquipedItemImage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = throwableEquipedItem.stack.ToString();
         }
     }
+    public void ShopSelectItem(int slot)
+    {
+        selectedItem = inventory.items[slot];
+        Image.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[slot].icon;
+        Text.GetComponent<TextMeshProUGUI>().text = inventory.items[slot].itemDescription;
+    }
     public void SelectItem(int slot)
     {
         selectedItem = inventory.items[slot];
-       // Image.GetComponent<UnityEngine.UI.Image>().sprite = inventory.items[slot].icon;
-        Text.GetComponent<TextMeshProUGUI>().text = inventory.items[slot].itemDescription;
-        if(1<= slot && slot <= 5) { ConsumableEquip(); }
-        if(6<= slot && slot <= 9) { ThrowableEquip(); }
+        if(1<= slot && slot <= 5) {
+            consumableGrid.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = inventory.items[slot].itemDescription;
+            ConsumableEquip();
+        }
+        if(6<= slot && slot <= 9) {
+            throwableGrid.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = inventory.items[slot].itemDescription;
+            ThrowableEquip(); 
+        }
     }
     public void WhenRemoved(int type)
     {
         //0 consumable 1 throwable
-        if (type ==0){consumableEquipedItem = inventory.nullitem; }
-        else { throwableEquipedItem = inventory.nullitem; }
+        if (type ==0){
+            consumableGrid.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = inventory.nullitem.itemDescription;
+            consumableEquipedItem = inventory.nullitem; 
+        }
+        else {
+            throwableGrid.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = inventory.nullitem.itemDescription;
+            throwableEquipedItem = inventory.nullitem;
+        }
         
        // Image.GetComponent<UnityEngine.UI.Image>().sprite = inventory.nullitem.icon;
-        Text.GetComponent<TextMeshProUGUI>().text = inventory.nullitem.itemDescription;
+       // Text.GetComponent<TextMeshProUGUI>().text = inventory.nullitem.itemDescription;
         UpdateInfo();
     }
     public Item GetSelectedItem()
